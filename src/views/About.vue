a0b16f28daf278b261a6991e3ead3a7095513c5b
kantachat.int <kantachat.int@bangkoklife.com>
2022-08-03 20:21:24 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Menu></Menu>
+    <div class="container mt-4">
+      <h1 class="mb-3">About Page</h1>
+      <p>gdsfghjsdf; sdf;otigdjgfsdirb eareioqpwer </p>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Menu from "@/components/Menu.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "About",
+  components: {
+    Menu,
+    Footer
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Menu></Menu>
+    <div class="container mt-4">
+      <h1 class=mb-4>Home Page</h1>
+      <p>Documentation and examples for Bootstrap’s powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more, including support for our collapse plugin.</p>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Menu from "@/components/Menu.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "Home",
+  components: {
+    Menu,
+    Footer
+  },
+};
+</script>
